1be62f861fea3aae7fcc903a62e05a1c93c9cefc
what? <jack.weller@hotmail.com>
2022-10-31 17:01:39 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-var theme = "dark";
-            
-function setTheme() {
-  if (theme == "dark") {
-    theme = "light"
-    document.documentElement.style.setProperty('--background', '#f5f6f7');
-    document.documentElement.style.setProperty('--foreground', '#101010');
-  }
-  if (theme == "light") {
-    theme = "dark"
-    document.documentElement.style.setProperty('--foreground', '#f5f6f7');
-    document.documentElement.style.setProperty('--background', '#101010');
-  }
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+    document.body.classList.add("dark-theme");
+} 
+else {
+    document.body.classList.remove("dark-theme");
 }
